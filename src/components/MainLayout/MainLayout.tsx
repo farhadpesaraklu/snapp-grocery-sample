74f7fa951ec68c36e771e0056d167d9e944ac98c
farhad <farhadpesaraklu@gmail.com>
2024-04-14 18:46:16 +0330

    Create layout

---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import './MainLayout.css';
+import { ArrowRight } from "iconsax-react";
+import backIcon from "../../assets/Images/headerChevron.svg"
+import snappExpressLogo from "../../assets/Images/expressLogo.svg"
+import { RouteEnums } from "../../utils/enums/routeEnum";
+import { useNavigate } from "react-router-dom";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="header-container">
+        <div className="back-icon" onClick={() => navigate(-1)}>
+          <img src={backIcon} alt="back" />
+        </div>
+        <a className="express-logo" href={`${RouteEnums.SUPERMARKET}`}>
+          <img src={snappExpressLogo} alt="snapp express" />
+        </a>
+      </div>
+      <div className="children-container">{children}</div>
+    </>
+  );
+};
