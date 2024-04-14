77deba6a2ff712305aa07f2eb79bf62f133353f4
farhad <farhadpesaraklu@gmail.com>
2024-04-14 18:47:30 +0330

    Create Supermarket page

---
+++ src/pages/supermarket/Supermarket.tsx
@@ -0,0 +1,12 @@
+import SearchAndSort from "../../components/searchAndSort/SearchAndSort"
+import SearchBar from "../search/Search"
+
+const Supermarket = () => {
+    return (
+        <div>
+            <SearchAndSort />
+        </div>
+    )
+}
+
+export default Supermarket
