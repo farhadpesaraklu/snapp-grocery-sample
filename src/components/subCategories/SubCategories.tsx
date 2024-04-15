2e949cf1674b7d436e15e37e50f1001b103b329a
Farhad Pesaraklu <farhadpesaraklu@gmail.com>
2024-04-16 02:37:49 +0330

    Connect subCategories component to category service

---
+++ src/components/subCategories/SubCategories.tsx
@@ -0,0 +1,53 @@
+import { FC, useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router";
+
+import "./SubCategories.css";
+import { Category } from "../../services/vendorProductCategoryService/type";
+
+interface SubCategoriesProps {
+  items: Category[];
+  handleClickSubCategory: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+const SubCategories: FC<SubCategoriesProps> = ({ items, handleClickSubCategory }) => {
+  const itemRefs = useRef<Record<string, HTMLDivElement | null>>({});
+
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(
+    Number(sessionStorage.getItem("selectedCategoryId")),
+  );
+
+  const location = useLocation();
+  const handleItemClick = (item: Category) => {
+    setSelectedItemId(item.id);
+    sessionStorage.setItem("selectedCategoryId", item.id.toString());
+    handleClickSubCategory(item.id);
+  };
+
+  useEffect(() => {
+    if (selectedItemId) {
+      const selectedItemRef = itemRefs.current[selectedItemId];
+
+      if (selectedItemRef) {
+        selectedItemRef.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [selectedItemId]);
+
+  return (
+    <div className="scrollable-container">
+      {items.map((item) => (
+        <div
+          ref={(ref) => (itemRefs.current[item.id] = ref)}
+          key={item.id}
+          className={`item ${selectedItemId === item.id ? "selected" : ""}`}
+          data-id={item.id}
+          onClick={() => handleItemClick(item)}
+        >
+          <span className="product-name">{item.title}</span>
+          <img src={item.image} alt="pic" width={48} height={48} />
+        </div>
+      ))}
+    </div>
+  );
+};
+export default SubCategories;
