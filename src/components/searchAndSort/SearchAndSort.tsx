42de3b5cc25945eb6beceed5d5fdc29acea8bf08
farhad <farhadpesaraklu@gmail.com>
2024-04-15 17:38:06 +0330

    Add onClick

--- src/components/searchAndSort/SearchAndSort.tsx
+++ src/components/searchAndSort/SearchAndSort.tsx
@@ -3,18 +3,21 @@ import "./SearchAndSort.css";
 import sortIcon from "../../assets/Images/sort.svg"
 import settingIcon from "../../assets/Images/setting.svg"
 import LabeledIcon from "../shared/labeledIcon/LabeledIcon";
+import { useNavigate } from "react-router-dom";
+import { RouteEnums } from "../../utils/enums/routeEnum";
 
 const SearchAndSort = () => {
+    const navigate = useNavigate()
+
     return (
-        <div className="search-and-sort-container" >
-            <div className="search-icon" >
-                <LabeledIcon icon={SearchIcon} width={28} height={28} />
+        <div className="search-and-sort-container">
+            <div className="search-icon">
+                <LabeledIcon icon={SearchIcon} width={28} height={28} onClick={() => navigate(`/${RouteEnums.SEARCH}`)} />
             </div>
 
-
             <div className="sort-container">
-                <LabeledIcon icon={sortIcon} title="مرتب سازی" />
-                <LabeledIcon icon={settingIcon} title="فیلتر" />
+                <LabeledIcon icon={sortIcon} title="مرتب سازی" onClick={() => null} />
+                <LabeledIcon icon={settingIcon} title="فیلتر" onClick={() => null} />
             </div>
         </div>
     )
