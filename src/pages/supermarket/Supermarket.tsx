f95efda5455686ed9e9d0177371e0f705e9b80cd
farhad <farhadpesaraklu@gmail.com>
2024-04-15 15:45:59 +0330

    Add mock data and import Categories

--- src/pages/supermarket/Supermarket.tsx
+++ src/pages/supermarket/Supermarket.tsx
@@ -1,12 +1,18 @@
 import SearchAndSort from "../../components/searchAndSort/SearchAndSort"
-import SearchBar from "../search/Search"
+import Categories from "../../components/categories/Categories";
 
 const Supermarket = () => {
     return (
         <div>
-            <SearchAndSort />
+            <SearchAndSort/>
+            <Categories items={tempCategory}/>
         </div>
     )
 }
 
 export default Supermarket
+
+const tempCategory = [{id: "1", text: "موادغذایی"}, {id: "2", text: "پوشاک کودک"}, {id: "3", text: "اسباب بازی کودک"}, {
+    id: "4",
+    text: "بهداشت کودک"
+}, {id: "5", text: "پوشاک"}, {id: "6", text: "پوشاک"}, {id: "7", text: "پوشاک"}, {id: "8", text: "پوشاک"}]
