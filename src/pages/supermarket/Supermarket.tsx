5e17671f059afad512656b40dfaa09d3ea0ef559
Farhad Pesaraklu <farhadpesaraklu@gmail.com>
2024-04-16 23:23:43 +0330

    Add useEffect for refetch data

--- src/pages/supermarket/Supermarket.tsx
+++ src/pages/supermarket/Supermarket.tsx
@@ -5,27 +5,35 @@ import SubCategories from "../../components/subCategories/SubCategories";
 import "./Supermarket.css";
 import useCategory from "../../hooks/useCategory";
 import useInfiniteLoading from "../../utils/hooks/useInfiniteLoading";
-import ProductList from "../../components/ProductList/ProductList";
 import { TailSpin } from "react-loader-spinner";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
+import ProductList from "../../components/productList/ProductList";
 
 const Supermarket = () => {
+  const [selectedSubCategoryId, setSelectedSubCategoryId] = useState<number | null>(
+    Number(sessionStorage.getItem("selectedCategoryId")) ?? null,
+  );
 
-  const [selectedSubCategoryId, setSelectedSubCategoryId] = useState<number | null>(Number(sessionStorage.getItem("selectedCategoryId")) ?? null);
-
-  const uniqueFiltersSet = (useSelector((state: RootState) => state.filterList.sortValue))
+  const uniqueFiltersSet = useSelector((state: RootState) => state.filterList.sortValue);
+  const selectedFiltersList = useSelector((state: RootState) => state.filterList.filterValuesList);
 
   const {
     vendorCategoryData,
     vendorCategoryDataFetchNextPage,
     vendorCategoryDataIsFetchingNextPage,
+    refetchVendorCategoryData,
   } = useCategory({
     vendor_code: "3eze12",
     category_id: "731207",
     subcat_id: selectedSubCategoryId || undefined,
-    filters: [uniqueFiltersSet]
+    filters: [...selectedFiltersList, uniqueFiltersSet],
   });
+  console.log("uniqueFiltersSet", uniqueFiltersSet);
+
+  useEffect(() => {
+    refetchVendorCategoryData();
+  }, [uniqueFiltersSet, selectedFiltersList]);
 
   useInfiniteLoading({
     scrollingElementSelector: "#list-container",
@@ -33,16 +41,18 @@ const Supermarket = () => {
     loaderMethod: vendorCategoryDataFetchNextPage,
     loading: vendorCategoryDataIsFetchingNextPage,
   });
+  const filtersAndSortList = useSelector((state: RootState) => state.filterList.filtersAndSortList);
+  console.log("filtersAndSortList", filtersAndSortList);
 
   return (
     <div className="supermarket-container">
-      <TopToolbar />
+      <TopToolbar data={filtersAndSortList} />
       {vendorCategoryData ? (
         <SubCategories
           items={vendorCategoryData?.pages?.[0]?.extra_sections?.categories}
           handleClickSubCategory={setSelectedSubCategoryId}
-        />)
-        :
+        />
+      ) : (
         <TailSpin
           visible={true}
           height="80"
@@ -52,8 +62,7 @@ const Supermarket = () => {
           radius="1"
           wrapperClass="TailSpin-container"
         />
-
-      }
+      )}
       <div className="product-list-container" id="list-container">
         {vendorCategoryData && <ProductList data={vendorCategoryData.pages} />}
       </div>
