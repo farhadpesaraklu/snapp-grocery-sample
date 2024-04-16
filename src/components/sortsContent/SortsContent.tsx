fd40e2c9aee1f86689a13e9a42ee8c6348b0a24f
farhad <farhadpesaraklu@gmail.com>
2024-04-16 15:02:28 +0330

    Add handleClick

--- src/components/sortsContent/SortsContent.tsx
+++ src/components/sortsContent/SortsContent.tsx
@@ -3,15 +3,29 @@ import { FC } from "react";
 import "./SortsContent.css";
 
 interface SortsContentProps {
-  onClose: () => void;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const SortsContent: FC<SortsContentProps> = (onClose) => {
+const SortsContent: FC<SortsContentProps> = ({ setOpen }) => {
+
+  const handleSortChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedValue = event.target.value;
+    setOpen(false);
+  };
+
   return (
-    <div>
-      <div className="sort-container">
+    <div className="sort-container">
+      <div className="sort-row">
         <span>مرتب سازی</span>
-        <CloseCircle size={24} />
+        <CloseCircle size={24} onClick={() => setOpen(false)} />
+      </div>
+      <div className="sort-row">
+        <span>ارزان ترین</span>
+        <input type="checkbox" className="radio" onChange={handleSortChange} value="least_expensive" />
+      </div>
+      <div className="sort-row">
+        <span>گران ترین</span>
+        <input type="checkbox" className="radio" onChange={handleSortChange} value="most_expensive" />
       </div>
     </div>
   );
