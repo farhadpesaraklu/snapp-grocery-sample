35dcec8f511ae168c3ca0a653415cbda72687936
farhad <farhadpesaraklu@gmail.com>
2024-04-16 18:45:06 +0330

    Add redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import AppRouter from './pages/AppRoutes';
+import { Provider } from 'react-redux';
+import { persistor, store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const queryClient = new QueryClient();
 
@@ -9,7 +12,11 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={AppRouter} />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <RouterProvider router={AppRouter} />
+        </PersistGate>
+      </Provider>
     </QueryClientProvider>
   );
 }
