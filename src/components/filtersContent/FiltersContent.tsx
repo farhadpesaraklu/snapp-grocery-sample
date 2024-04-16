2e5f9dbccf5e8016399a3c5e5edb2b839bbbccdc
farhad <farhadpesaraklu@gmail.com>
2024-04-16 19:03:23 +0330

    Remove unused import

--- src/components/filtersContent/FiltersContent.tsx
+++ src/components/filtersContent/FiltersContent.tsx
@@ -1,6 +1,7 @@
 import { CloseCircle } from "iconsax-react";
 import { FC, useState } from "react";
 import "./FiltersContent.css"
+import GroupButton from "../groupButton/GroupButton";
 interface FiltersContentProps {
   setIsFilterOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
@@ -17,22 +18,31 @@ const FiltersContent: FC<FiltersContentProps> = ({ setIsFilterOpen }) => {
 
   return (
     <div className="filter-container">
-      <div className="filter-row">
-        <span className="filter-title">فیلترها</span>
-        <CloseCircle size={24} onClick={() => setIsFilterOpen(false)} />
+      <div>
+        <div className="filter-row">
+          <span className="filter-title">فیلترها</span>
+          <CloseCircle size={24} onClick={() => setIsFilterOpen(false)} />
+        </div>
+        <div className="filter-row">
+          <span className="filter-normal-text">موجود</span>
+          <label className="switch">
+            <input type="checkbox" checked={isChecked} onClick={() => setIsChecked(!isChecked)} />
+            <span className="slider round"></span>
+          </label>
+        </div>
+        <div className="filter-row">
+          <span className="filter-normal-text">دارای تخفیف</span>
+          <label className="switch">
+            <input type="checkbox" checked={isChecked} onClick={() => setIsChecked(!isChecked)} />
+            <span className="slider round"></span>
+          </label>
+        </div>
       </div>
-      <div className="filter-row">
-        <span className="filter-normal-text">موجود</span>
-        <input type="radio" className="radio" onChange={handleSortChange} value="has_stock" />
+      <div className="container-btn">
+        <button className='delete-btn'  >حذف همه</button>
+        <button className='action-btn'  >اعمال</button>
       </div>
-      <div className="filter-row">
-        <span className="filter-normal-text">دارای تخفیف</span>
-        {/* <input type="radio" className="radio" onChange={handleSortChange} value="has_discount" /> */}
-        <label className="switch">
-          <input type="checkbox" checked={isChecked} onClick={() => setIsChecked(!isChecked)} />
-          <span className="slider round"></span>
-        </label>
-      </div>
+
     </div>
   );
 };
